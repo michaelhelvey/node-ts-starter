b1655c5e258a5cd730dd2600d14623b14d27e8c7
Michael Helvey <michael@michaelhelvey.dev>
2022-10-02 15:36:01 -0500

    Be very permissive about typescript in tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,44 @@
+const typescriptEslintPlugin = require('@typescript-eslint/eslint-plugin')
+
 /** @type {import('@types/eslint').Linter.BaseConfig}*/
 module.exports = {
 	root: true,
-	parser: "@typescript-eslint/parser",
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaVersion: "latest",
+		ecmaVersion: 'latest',
 		tsconfigRootDir: __dirname,
-		project: ["./tsconfig.json"],
+		project: ['./tsconfig.json'],
 	},
-	plugins: ["@typescript-eslint", "jest"],
+	plugins: ['@typescript-eslint', 'jest'],
 	extends: [
-		"eslint:recommended",
-		"prettier",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		'eslint:recommended',
+		'prettier',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 	],
+	rules: {
+		'no-console': ['warn'],
+	},
+	// While we use vitest instead of jest, because of the very similar API, we
+	// can still make good use of jest's linting plugin
 	settings: {
 		jest: {
 			version: 28,
 		},
 	},
-};
+	overrides: [
+		{
+			files: ['*.test.*'],
+			rules: {
+				// Be extremely permissive about our use of typescript in test files
+				...Object.keys(typescriptEslintPlugin.rules).reduce(
+					(table, rule) => {
+						table[`@typescript-eslint/${rule}`] = ['off']
+						return table
+					},
+					{}
+				),
+			},
+		},
+	],
+}
